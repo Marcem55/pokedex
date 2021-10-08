c099935d447b5c0b0c5c159d83a21c68f293745c
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-10-08 20:12:06 -0300

    here we go again

---
+++ api/src/constants/constants.js
@@ -0,0 +1,4 @@
+export const generalUrl = 'https://pokeapi.co/api/v2/pokemon';
+export const urlById = 'https://pokeapi.co/api/v2/pokemon/{id}';
+export const urlByName = 'https://pokeapi.co/api/v2/pokemon/{name}';
+export const urlType = 'https://pokeapi.co/api/v2/type';
