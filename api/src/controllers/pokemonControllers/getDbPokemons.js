1a7b1d546dbe89146c7f0404e467b2a72e69e748
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-10-11 20:55:55 -0300

    db pokemons

---
+++ api/src/controllers/pokemonControllers/getDbPokemons.js
@@ -0,0 +1,19 @@
+const { Pokemon, Type } = require('../../db');
+
+const getDbPokemons = async (req, res, next) => {
+    try {
+        const dbPokemons = await Pokemon.findAll({
+            include: {
+                model: Type,
+                attributes: ['name']
+            }
+        });
+        return dbPokemons;
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    getDbPokemons
+}
--- api/src/routes/pokemons.js
+++ api/src/routes/pokemons.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { getApiPokemons } = require('../controllers/pokemonControllers/getApiPokemons');
+const { getDbPokemons } = require('../controllers/pokemonControllers/getDbPokemons');
 
 const router = Router();
 
 router.get('/', getApiPokemons);
+router.get('/', getDbPokemons);
 
 module.exports = router;
