e48e3a2bb55e58de01688b2bab468937075111e0
Marcelo Malacalza <MalacalzaMarcelo@gmail.com>
2021-10-20 18:05:16 -0300

    typeController fixed

--- api/src/controllers/typesControllers.js
+++ api/src/controllers/typesControllers.js
@@ -4,11 +4,10 @@ const { Type } = require('../db');
 const getAllTypes = async () => {
     try {
         let dbTypeNames;
-
         let typesInDb = await Type.findAll();
 
         if (typesInDb.length === 0) {
-            // me traigos los tipos de la API
+            // al no encontrar en la base de datos, me traigos los tipos de la API
             const apiTypes = await axios.get('https://pokeapi.co/api/v2/type');
             const apiTypesResults = apiTypes.data.results;
 
@@ -30,32 +29,7 @@ const getAllTypes = async () => {
     catch (err) {
         console.log(err);
     }
-    // try {
-    //     const dbTypes = await Type.findAll({
-    //         attributes: ['name']
-    //     });
-    //     if(dbTypes.length === 0){
-    //         const apiTypes = await axios.get('https://pokeapi.co/api/v2/type');
-    //         let createdTypes = apiTypes.data.results.map(async type => await Type.create({name: type.name}));  //me guardo los types en DB
-    //         createdTypes = await axios.all(createdTypes);
-    //         console.log(createdTypes);
-    //         const getApiTypes = getTypes(createdTypes);
-    //         return getApiTypes;
-    //     }else{
-    //         const getDbTypes = getTypes(dbTypes);
-    //         console.log(getDbTypes);
-    //         return getDbTypes;
-    //     }
-    // } catch (error) {
-    //     console.log(error);
-    //     return error;
-    // }
 };
-
-// const getTypes = (array) => {
-//     let types = array.map( type => type.name);
-//     return types;
-// }
 
 module.exports = {
     getAllTypes
